56c773590b9fd3483e17779393e6f22314880787
Renata445 <kisa09-10@mail.ru>
2023-04-22 06:20:17 +0300

    src

---
+++ src/seminar3/Main.java
@@ -0,0 +1,13 @@
+package seminar3;
+
+import seminar3.task.task1;
+import seminar3.task.task2;
+import seminar3.task.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        task1 task1 = new task1();
+        task2 task2 = new task2();
+        task3 task3 = new task3();
+    }
+}
